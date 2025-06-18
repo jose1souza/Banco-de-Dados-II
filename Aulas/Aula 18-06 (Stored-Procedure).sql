6b2d84ec1bb0b13de19b6df6e0dd6409e61173e0
José Carlos Souza <166337638+jose1souza@users.noreply.github.com>
2025-06-18 09:32:22 -0300

    :feat(sp): adiciona novas stored procedures para cadastro de clientes

--- Aulas/Aula 18-06 (Stored-Procedure).sql
+++ Aulas/Aula 18-06 (Stored-Procedure).sql
@@ -10,4 +10,49 @@ BEGIN
 		venda.Cliente_codCliente = cliente.codCliente;
 END //
 DELIMITER ;
-CALL sp_desconto_total_cliente(1,@Total);
+select * from venda;
+SELECT SUM(desconto)
+	FROM venda,cliente
+		WHERE venda.cliente_codCliente = 6 AND
+		venda.Cliente_codCliente = cliente.codCliente;
+CALL sp_desconto_total_cliente(5,@desconto);
+select @desconto;
+SELECT * FROM marca;
+DELIMITER //
+CREATE PROCEDURE sp_guarda_marca(
+	IN p_nome VARCHAR(100),
+    OUT p_nome_guardado VARCHAR(100))
+    BEGIN
+		SELECT nome INTO p_nome_guardado
+        FROM marca WHERE nome = p_nome LIMIT 1;
+END //
+DELIMITER ;
+CALL sp_guarda_marca("Garoto",@nomeRecebido);
+-----------------------------------------------------
+-- Usando outro banco
+use bancobd22025;
+DELIMITER //
+CREATE PROCEDURE sp_inserir_cliente(
+	IN p_nome_cliente VARCHAR(150),
+    IN p_cpf_cliente VARCHAR(45),
+    IN p_nascimento_cliente DATE
+)
+BEGIN 
+	IF (
+    p_nome_cliente IS NULL OR 
+    TRIM(p_nome_cliente) = '' OR
+    p_cpf_cliente IS NULL OR 
+    TRIM(p_cpf_cliente) = '' OR
+    p_nascimento_cliente IS NULL OR 
+    TRIM(p_nascimento_cliente) = ''
+    ) THEN
+		SELECT "Campos obrigatórios!" AS msg;
+	ELSE 
+		INSERT INTO cliente (nome, cpf, dataNascimento)
+			VALUES
+				(p_nome_cliente,p_cpf_cliente,p_nascimento_cliente);
+    END IF;
+END //
+DELIMITER ;
+CALL sp_inserir_cliente("José","156.788.123-54",NULL);
+SELECT * FROM cliente;
